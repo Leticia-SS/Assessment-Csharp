6d6d620f83bce3f96f37d213085e86700a9a6479
Leticia-SS <leticia.saraiva013@gmail.com>
2025-03-23 22:38:27 -0300

    exercicio 02

--- exercicio02/Program.cs
+++ exercicio02/Program.cs
@@ -7,7 +7,23 @@
 
         string[] arrayFrase = frase.Split(' ');
 
-        
+
+        foreach (string palavra in arrayFrase)
+        {
+            Console.WriteLine(palavra);
+            string novaPalavra = "";
+            for (int i = 0; i < palavra.Length; i++)
+            {
+                char letra = (char)(palavra[i] + 2);
+                Console.Write(letra);
+                novaPalavra += letra;
+            }
+            Console.WriteLine(novaPalavra);
+
+        }
+
+        Console.WriteLine(arrayFrase[0]);
+        Console.WriteLine(arrayFrase[1]);
 
     }
 }
