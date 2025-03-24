25ef18633abac92eec7b938ef1609b616e47e58b
Leticia-SS <leticia.saraiva013@gmail.com>
2025-03-24 11:10:25 -0300

    classe ContaBancaria do exercicio07

---
+++ exercicio07/ContaBancaria.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio07
+{
+    internal class ContaBancaria
+    {
+        public string _Titular;
+        private double _Saldo;
+
+        //Construtor para criar objeto inserindo os parametros
+        public ContaBancaria(string titular, double saldo)
+        {
+            _Titular = titular;
+            _Saldo = saldo;
+        }
+
+        // Método Depositar
+        public double Depositar(double valor)
+        {
+            return _Saldo + valor;
+        }
+
+        // Método Sacar
+        public double Sacar(double valor)
+        {
+            return _Saldo - valor;
+        }
+
+
+
+
+
+
+    }
+}
