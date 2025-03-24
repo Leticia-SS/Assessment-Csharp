dd8e755ca29d684e73c9b6f85438a3f0a47da1c9
Leticia-SS <leticia.saraiva013@gmail.com>
2025-03-23 23:03:39 -0300

    adding comments to exercicio02

--- exercicio02/Program.cs
+++ exercicio02/Program.cs
@@ -5,33 +5,42 @@
         Console.WriteLine("Digite uma frase");
         string frase = Console.ReadLine();
 
+        // Criando um array com cada palavra da frase como item
         string[] arrayFrase = frase.Split(' ');
 
-        Console.WriteLine("");
+        Console.WriteLine(""); // Pulando uma linha
 
+        // Printando a frase antes da soma de 2 letras em cada caracter
         Console.Write("Frase antes: ");
         foreach(string palavra in arrayFrase)
         {
             Console.Write(palavra + " ");
         }
 
-        Console.WriteLine("");
+        Console.WriteLine(""); // Pulando uma linha
 
+        // Iterando cada palavra dentro do array
         Console.Write("Frase depois: ");
         for (int n= 0; n < arrayFrase.Length; n++)
         {
+            // Criando uma variável para adicionar as letras modificadas formando uma palavra
             string novaPalavra = "";
+
+            // Iterando cada palavra e modificando cada letra
             for (int i = 0; i < arrayFrase[n].Length; i++)
             {
                 string palavra = arrayFrase[n];
                 char letra = (char)(palavra[i] + 2);
                 novaPalavra += letra;
             }
+
+            // Salvando a palavra atual no array na posição da palavra antiga
             arrayFrase[n] = novaPalavra;
+            // Printando a palavra modificada 
             Console.Write(novaPalavra + " ");
 
         }
-        Console.WriteLine("");
+        Console.WriteLine(""); // Pulando uma linha
 
     }
 }
